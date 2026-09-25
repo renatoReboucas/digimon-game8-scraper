13a832214b9bbab5bc9250bc19d3fa46d517e520
Renato Rebouças <renato190698@gmail.com>
2026-09-25 17:13:13 -0300

    fix: corrigir URL no componente Link

--- next-app/src/components/link.tsx
+++ next-app/src/components/link.tsx
@@ -6,7 +6,7 @@ import type {Evolution} from '@/types/DigimonTypes'
 const WIKI_URL = 'https://www.grindosaur.com/en/games/digimon-story-time-stranger/digimon'
 export function Link({ item }:{item: Evolution}) {
   return (
-    <a className="game8-link" href={`WIKI_URL/${item.name}`} target="_blank" rel="noreferrer" title="Abrir no grindosaur">
+    <a className="game8-link" href={`${WIKI_URL}/${item.name}`} target="_blank" rel="noreferrer" title="Abrir no grindosaur">
       <SquareArrowOutUpRight aria-hidden="true" size={15} strokeWidth={1.8} />
     </a>
   );
